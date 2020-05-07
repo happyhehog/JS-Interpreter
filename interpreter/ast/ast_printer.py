79e30208c7906cc34b2ad5ba9c17b0fee58a0150
Evgeny Schukin <yevgeny2525@gmail.com>
2020-05-07 20:29:31 +1000

    [interpreter/ast]: add get_str_ast method

--- interpreter/ast/ast_printer.py
+++ interpreter/ast/ast_printer.py
@@ -1,3 +1,4 @@
+import os
 import typing
 from interpreter.parser import JavaScriptParser
 from interpreter.ast.ast_visitor import ASTVisitor
@@ -20,3 +21,14 @@ def print_ast(ast_nodes: ast_nodes.Program) -> typing.NoReturn:
     ast_viewver = ASTView()
     ast_viewver.visit(ast_nodes)
     ast_viewver.ast_tree.show(key=False, line_type="ascii")
+
+
+def get_str_ast(ast_nodes: ast_nodes.Program) -> str:
+    ast_viewver = ASTView()
+    ast_viewver.visit(ast_nodes)
+    # Package doesn't allow you to return a string with sorted nodes, so you have to do this.
+    # https://github.com/caesar0301/treelib/issues/111
+    ast_viewver.ast_tree.save2file("temp", key=False, line_type="ascii") #
+    result = open("temp").read()
+    os.remove("temp")
+    return result
