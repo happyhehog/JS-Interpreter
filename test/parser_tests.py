ff3a234a46853e4bb8bc12d43acf078c028b8215
Evgeny Schukin <yevgeny2525@gmail.com>
2020-05-07 14:25:57 +1000

    [tests]: add tests for ast

--- test/parser_tests.py
+++ test/parser_tests.py
@@ -6,16 +6,16 @@ def read_answer_from_file(path_to_file):
 
 
 def test_variables_parsing():
-    file_path = "test_files/variables.js"
+    file_path = "test_parser_files/variables.js"
     parsed_tuple = interpreter.parser.parse_file(file_path)
     parser = parsed_tuple[0]
     tree = parsed_tuple[1]
-    assert tree.toStringTree(parser.ruleNames, parser) == read_answer_from_file("test_files/variables.answer")
+    assert tree.toStringTree(parser.ruleNames, parser) == read_answer_from_file("test_parser_files/variables.answer")
 
 
 def test_fibo_parsing():
-    file_path = "test_files/fibo.js"
+    file_path = "test_parser_files/fibo.js"
     parsed_tuple = interpreter.parser.parse_file(file_path)
     parser = parsed_tuple[0]
     tree = parsed_tuple[1]
-    assert tree.toStringTree(parser.ruleNames, parser) == read_answer_from_file("test_files/fibo.answer")
+    assert tree.toStringTree(parser.ruleNames, parser) == read_answer_from_file("test_parser_files/fibo.answer")
